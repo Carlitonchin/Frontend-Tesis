9ed9da4feded5c70f36803d5a1d4ea7896ac6ed5
Carlitonchin <carlozalejandro98@gmail.com>
2022-07-16 13:26:38 -0400

    first commit

---
+++ layouts/default.vue
@@ -0,0 +1,16 @@
+<script setup>
+useHead({
+  title: 'My App',
+  // or, instead:
+  // titleTemplate: (title) => `My App - ${title}`,
+  viewport: 'width=device-width, initial-scale=1, maximum-scale=1',
+  charset: 'utf-8',
+  meta: [
+    { name: 'description', content: 'My amazing site.' }
+  ]
+})
+</script>
+
+<template>
+    <slot/>
+</template>
---
+++ pages/index.vue
@@ -0,0 +1,7 @@
+
+
+<template>
+<NuxtLayout>
+  <h1 class="logo"><span class="first-color">Ask</span>Matcom</h1>
+</NuxtLayout>
+</template>
